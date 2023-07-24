4c87810932fd494350bb5669d1a45eaaf1f5e945
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-24 00:58:44 +0000

    Add correct heroku link to ALLOWED_HOSTS

--- letseat/settings.py
+++ letseat/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['', '8000-mristimaki-letseatpp4-afasvru6gqt.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['lets-eat-0870a5c41672.herokuapp.com', '8000-mristimaki-letseatpp4-afasvru6gqt.ws-eu102.gitpod.io']
 
 
 # Application definition
