a5201463cc20006230a62687145f604a8e16dd07
mristimaki <rosanna.ristimaki@gmail.com>
2023-07-25 01:26:44 +0000

    Add Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=80, unique=True)
+    slug = models.SlugField(max_length=400, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    num_comments = models.ManyToManyField(User, related_name='blog_comments', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def number_of_comments(self):
+        return self.comments.count()
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
