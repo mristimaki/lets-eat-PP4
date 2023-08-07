8df51e410631f9d203d062642e2353f5df5d9f92
mristimaki <rosanna.ristimaki@gmail.com>
2023-08-07 00:58:30 +0000

    Add PostLikes view as well as changed some post details in index.html

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name="post_detail"),
+    path('like/<slug:slug>', views.PostLikes.as_view(), name="post_likes"),
 ]
