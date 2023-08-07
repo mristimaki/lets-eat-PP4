0e9fa2e84bbae72a654baadaf70635f88a508e62
mristimaki <rosanna.ristimaki@gmail.com>
2023-08-07 17:30:48 +0000

    Add alert messages along with js to close message after 3sec

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -56,6 +57,7 @@ class RecipeDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.success(request, 'Thank you for connecting with us!')
         else:
             comment_form = CommentForm()
         
