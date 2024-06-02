bf3ad2652bdc532253e015da9dc49920e0dfb36d
mristimaki <rosanna.ristimaki@gmail.com>
2024-06-02 13:25:20 +0000

    Updated edit_comment.html and post_detail.html with edit comment form

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
+from .views import EditComment
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name="post_detail"),
     path('like/<slug:slug>', views.PostLikes.as_view(), name="post_likes"),
+    path('edit_comment/<int:comment_id>', EditComment.as_view(), name='edit_comment'),
 ]
